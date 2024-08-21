264f21ba15fadc27eaa8bdfb6dff79445e7762f4
roxannejt83 <rjtaylor28@amarillocollege.com>
2024-08-21 12:55:13 -0500

    punctuation

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is Quiz 1");
+            Console.WriteLine("This is Quiz 1.");
 
             Console.Read();
         }
